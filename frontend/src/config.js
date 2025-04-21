e874bebe7647684ec6f481ca239bb9e6628aaf60
jmasciar82 <juanpsnusa@gmail.com>
2025-04-20 23:48:40 -0300

    segundo commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const backendURL = isProduction
-  ? 'https://dalt-trabajo-campo-backend.onrender.com/api' // URL de producción
+  ? 'https://barrio-back.onrender.com/api' // URL de producción
   : 'http://localhost:5075/api'; // URL de desarrollo local
 
 export default backendURL;
