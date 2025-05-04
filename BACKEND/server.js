4215659744ee9e34c1954994f074ce3f49f2f2e2
jmasciar82 <juanpsnusa@gmail.com>
2025-05-04 16:05:39 -0300

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -24,12 +24,10 @@ app.use(express.json());
 // Rutas de la API
 app.use('/api/reservations', reservationRoutes);
 
-// Si estamos en producción, servir los archivos estáticos de React
-if (process.env.NODE_ENV === 'production') {
-  // Servir archivos estáticos del build de React
+// Si se define SERVE_FRONTEND, servir archivos estáticos
+if (process.env.SERVE_FRONTEND === 'true') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  // Cualquier ruta que no sea de la API, devolver el index.html de React
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
