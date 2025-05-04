fee6fe80865e0570fea08d8ad7b6177774e1fd27
jmasciar82 <juanpsnusa@gmail.com>
2025-05-04 16:14:56 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './components/Welcome';  // Nota el ./ al inicio
-import Reservation from './components/Reservation';
+import Reservation from './components/Reservation.js';
 
 function App() {
   return (
