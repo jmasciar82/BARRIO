a2a34ede8dee88c93eab3c17ccb2ac7b039528cb
jmasciar82 <juanpsnusa@gmail.com>
2026-07-18 01:01:14 -0300

    chore: actualizar URL de backend en produccion a reserva-parrillas.onrender.com

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const backendURL = isProduction
-  ? 'https://barrio-back.onrender.com/api' // URL de producción
+  ? 'https://reserva-parrillas.onrender.com/api' // URL de producción
   : 'http://localhost:5075/api'; // URL de desarrollo local
 
 export default backendURL;
